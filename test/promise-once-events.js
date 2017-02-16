11abc8c9b24f0e38d3b33f7c337ceb8ab13ad0f4
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2017-02-16 23:15:23 +0100

    ES6

--- test/promise-once-events.js
+++ test/promise-once-events.js
@@ -1,43 +1,43 @@
 'use strict'
 
-var t = require('tap')
-var PromiseOnceEvents = require('../lib/promise-once-events')
+const t = require('tap')
+const PromiseOnceEvents = require('../lib/promise-once-events')
 
-var TIMEOUT = 10000
+const TIMEOUT = 10000
 
 t.plan(2)
 
-t.test('On event with callback', {timeout: TIMEOUT}, function (t) {
+t.test('On event with callback', {timeout: TIMEOUT}, t => {
   t.plan(4)
 
-  var ev = new PromiseOnceEvents()
+  const ev = new PromiseOnceEvents()
 
-  var promise = ev.once('event', function (arg1, arg2, resolve) {
+  const promise = ev.once('event', (arg1, arg2, resolve) => {
     t.equal(arg1, 'arg1', 'Received event with correct arg1')
     t.equal(arg2, 'arg2', 'Received event with correct arg2')
     t.type(resolve, 'function', 'Received event with resolve callback')
     resolve('result')
   })
 
-  promise.then(function (result) {
+  promise.then(result => {
     t.equal(result, 'result', 'Fulfilled promise with correct result')
-  }).then(function () {
+  }).then(() => {
     t.end()
   })
 
   ev.emit('event', 'arg1', 'arg2')
 })
 
-t.test('On event without callback', {timeout: TIMEOUT}, function (t) {
+t.test('On event without callback', {timeout: TIMEOUT}, t => {
   t.plan(1)
 
-  var ev = new PromiseOnceEvents()
+  const ev = new PromiseOnceEvents()
 
-  var promise = ev.once('event')
+  const promise = ev.once('event')
 
-  promise.then(function (result) {
+  promise.then(result => {
     t.same(result, ['arg1', 'arg2'], 'Fulfilled promise with correct result')
-  }).then(function () {
+  }).then(() => {
     t.end()
   })
 
